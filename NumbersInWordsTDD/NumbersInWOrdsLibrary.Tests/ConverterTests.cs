1a3ee31e7cecd89160ffd642c3d11ccb9fde2ba7
Roberto Monesi <roberto.monesi@outlook.com>
2019-01-13 10:22:12 +0000

    Add conversion for number between 0 and 19

--- NumbersInWordsTDD/NumbersInWOrdsLibrary.Tests/ConverterTests.cs
+++ NumbersInWordsTDD/NumbersInWOrdsLibrary.Tests/ConverterTests.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using NUnit.Framework;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,8 +7,44 @@ using System.Threading.Tasks;
 
 namespace NumbersInWordsLibrary.Tests
 {
+    [TestFixture]
     public class ConverterTests
     {
+        private Converter converter;
 
+        [SetUp]
+        public void Seup()
+        {
+            converter = new Converter();
+        }
+
+        #region TestCase_0To19
+        [TestCase(0, "zero")]
+        [TestCase(1, "one")]
+        [TestCase(2, "two")]
+        [TestCase(3, "three")]
+        [TestCase(4, "four")]
+        [TestCase(5, "five")]
+        [TestCase(6, "six")]
+        [TestCase(7, "seven")]
+        [TestCase(8, "eight")]
+        [TestCase(9, "nine")]
+        [TestCase(10, "ten")]
+        [TestCase(11, "eleven")]
+        [TestCase(12, "twelve")]
+        [TestCase(13, "thirteen")]
+        [TestCase(14, "fourteen")]
+        [TestCase(15, "fifteen")]
+        [TestCase(16, "sixteen")]
+        [TestCase(17, "seventeen")]
+        [TestCase(18, "eighteen")]
+        [TestCase(19, "nineteen")]
+        public void Convert_0_to_19_InWords(int input, string expected)
+        {
+            string output = converter.ConvertToWords(input);
+
+            Assert.AreEqual(expected, output);
+        }
+        #endregion
     }
 }
--- NumbersInWordsTDD/NumbersInWordsLibrary/Converter.cs
+++ NumbersInWordsTDD/NumbersInWordsLibrary/Converter.cs
@@ -6,7 +6,80 @@ using System.Threading.Tasks;
 
 namespace NumbersInWordsLibrary
 {
-    public class Class1
+    public class Converter
     {
+        /// <summary>
+        /// Convert the input numbers to words
+        /// </summary>
+        /// <param name="input"> positive number to convert</param>
+        /// <returns>number expressed in words</returns>
+        public string ConvertToWords(int input)
+        {
+            if (input >= 0 && input <= 19)
+            {
+                return Convert_From0To19_ToWords(input);
+            }
+            else
+            {
+                return null;
+            }
+
+        }
+
+        /// <summary>
+        /// Convert number from 0 to 19.
+        /// Explicit function because ther is no pattern for these
+        /// </summary>
+        /// <param name="input"></param>
+        /// <returns></returns>
+        public string Convert_From0To19_ToWords(int input)
+        {
+            switch (input)
+            {
+                case 0:
+                    return "zero";
+                case 1:
+                    return "one";
+                case 2:
+                    return "two";
+                case 3:
+                    return "three";
+                case 4:
+                    return "four";
+                case 5:
+                    return "five";
+                case 6:
+                    return "six";
+                case 7:
+                    return "seven";
+                case 8:
+                    return "eight";
+                case 9:
+                    return "nine";
+                case 10:
+                    return "ten";
+                case 11:
+                    return "eleven";
+                case 12:
+                    return "twelve";
+                case 13:
+                    return "thirteen";
+                case 14:
+                    return "fourteen";
+                case 15:
+                    return "fifteen";
+                case 16:
+                    return "sixteen";
+                case 17:
+                    return "seventeen";
+                case 18:
+                    return "eighteen";
+                case 19:
+                    return "nineteen";
+            }
+            return null;
+        }
+
+        
     }
 }
