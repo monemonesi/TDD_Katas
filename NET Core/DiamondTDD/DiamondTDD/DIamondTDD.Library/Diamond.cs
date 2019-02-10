1b64cacb720950db04613d722fc38c2c5bdecf87
Roberto Monesi <roberto.monesi@outlook.com>
2019-02-10 15:24:53 +0000

    Created the upper part of the diamond

--- NET Core/DiamondTDD/DiamondTDD/DIamondTDD.Library/Diamond.cs
+++ NET Core/DiamondTDD/DiamondTDD/DIamondTDD.Library/Diamond.cs
@@ -23,31 +23,49 @@ namespace DIamondTDD.Library
                 {
                     output += CreateFirstLine(letterId, i);
                 }
+                else
+                {
+                    output += CreateLine(letterId, i);
+                }
             }
 
 
             return output;
         }
 
-        private static string CreateFirstLine(int letterId, int lineId)
+        private static string CreateFirstLine(int letterId, int currentLine)
         {
-            return $"{LeftSpaces(letterId, lineId)}A\n";
+            return $"{CreateLeftSpaces(letterId, currentLine)}A\n";
         }
 
-        private static string LeftSpaces(int letterId, int lineId)
+        private static string CreateLeftSpaces(int letterId, int currentLine)
         {
             string leftSpaces = string.Empty;
-            for (int j = 0; j < (letterId-lineId); j++)
+            for (int j = 0; j < (letterId-currentLine); j++)
             {
                 leftSpaces += " ";
             }
             return leftSpaces;
         }
 
-        private static string CreateLine(int i)
+        private static string CreateLine(int letterId, int currentLine)
         {
 
-            return Alphabet[i].ToString();
+            return $"{CreateLeftSpaces(letterId, currentLine)}" +
+                    $"{Alphabet[currentLine]}" +
+                    $"{CreateCentralSpaces(letterId, currentLine)}" +
+                    $"{Alphabet[currentLine]}\n";
+
+        }
+
+        private static object CreateCentralSpaces(int letterId, int currentLine)
+        {
+            string centralSpaces = string.Empty;
+            for (int j = 0; j < (currentLine * 2 -1); j++)
+            {
+                centralSpaces += " ";
+            }
+            return centralSpaces;
         }
     }
 }
